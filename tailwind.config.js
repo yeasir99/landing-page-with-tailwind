5dbf4b2f8eaba039c96b79c80bd69dc4e429ed8f
yeasirahamed <yahamed007@gmail.com>
2021-04-11 23:25:13 +0600

    add svg wave and fix nav issue and custom height

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   purge: ["/**/*.html", "/**/*.js"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        custom: "var(--custom-height)",
+      },
+    },
   },
   variants: {
     extend: {},
